4707dbee8a1342c51a8da03dbbfca4295596f52a
Maahir Ahmed <maahirahmed2910@gmail.com>
2023-08-27 17:39:50 +1000

    test

--- Cricket_Scoring.py
+++ Cricket_Scoring.py
@@ -8,121 +8,121 @@ It should also keep track of outs, storing the bowler’s name whenever a batter
 """
 import customtkinter as ctk
 import random
-class App:
-    def __init__(self):
-       
-        self.app = ctk.CTk()  # Create the application window
-        self.app.title("My Cricket Application")  # Set the desired title
+from tkinter import messagebox as msg
+from tkinter import *
 
+class App(ctk.CTk): 
+
+    def __init__(self, *args, **kwargs):
+        ctk.CTk.__init__(self, *args, **kwargs)  # Create the application window
+        self.title("My Cricket Application")  # Set the desired title
+    
         #get screen dimensions
-        screen_width = self.app.winfo_screenwidth()
-        screen_height = self.app.winfo_screenheight()
-
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
         #set the window size to match the screen
-        self.app.geometry(f'{screen_width}x{screen_height}')
+        self.geometry(f'{screen_width}x{screen_height}')
+        self.resizable(False, False)
         
-        # Create the frames for ther application (ADD MORE FRAMES HERE)
-        self.team_selection_frame = ctk.CTkFrame(self.app)
-        self.toss_frame = TossFrame(self.team_selection_frame, team_names=[])  # Initially empty, updated during the toss
-        self.main_menu_frame = MainMenuFrame(self.app, self.team_selection_frame)
-        self.team_input_frame = TeamInputFrame(self.team_selection_frame)
+        app = ctk.CTkFrame(self) #first frame, fills root window, parent of all other frames/screens
+        app.pack(side="top", fill="both", expand=True)
+        app.grid_rowconfigure(0, weight=1)
+        app.grid_columnconfigure(0, weight=1)
 
-        self.main_menu_frame.play_button.configure(command=self.switch_to_team_selection)
+        # packs all frames/screens to app frame
+        self.frames = {}
+        for F in (MainMenuFrame, TeamInputFrame):
+            page_name = F.__name__
+            frame = F(app, self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
         
-    def switch_to_toss(self):
-        # Switch to the toss frame
-        self.main_menu_frame.pack_forget()
-        self.toss_frame.pack()
-        self.toss_frame.perform_toss()  # Automatically perform the toss
-        
-    def switch_to_scoring_frame(self, team_names):
-        self.toss_frame.pack_forget()
-        scoring_frame = ScoringFrame(self.team_selection_frame, team_names, [])
-        scoring_frame.back_to_toss_callback = self.switch_to_toss  # Set the callback function
-        scoring_frame.pack(padx=10, pady=10)
-
-    def run(self):
-        # Display the main menu frame
-        self.main_menu_frame.pack(padx=10, pady=10)
-        self.app.mainloop()  # Start the application main loop
-
-    def switch_to_team_selection(self):
-        # Switch to the team selection frame
-        self.main_menu_frame.pack_forget()
-        self.team_selection_frame.pack()
-        self.team_input_frame.pack(padx=10, pady=10)
+        # shows startscreen first (when __init__ runs)
+        self.show_frame("MainMenuFrame")
+    
+    # brings the parsed frame to the top (ie changes the displayed screen)
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 class MainMenuFrame(ctk.CTkFrame):
-    def __init__(self, master, team_selection_frame):
-        super().__init__(master)  # Initialize the parent class
+    
+    def __init__(self, parent, controller):
+        ctk.CTkFrame.__init__(self, parent, controller)
+        self.controller = controller
         self.grid_columnconfigure(0, weight=1)
-        self.team_selection_frame = team_selection_frame
 
         # Play button
-        self.play_button = ctk.CTkButton(self, text="Play")
-        self.play_button.pack(pady=10)
+        play_button = ctk.CTkButton(self, text="Play", command=lambda: (controller.show_frame("TeamInputFrame")))
+        play_button.pack(pady=10)
 
         # How to Use button
-        self.how_to_use_button = ctk.CTkButton(self, text="How to Use")
-        self.how_to_use_button.pack(pady=10)
+        how_to_use_button = ctk.CTkButton(self, text="How to Use")
+        how_to_use_button.pack(pady=10)
 
         # Quit button
-        self.quit_button = ctk.CTkButton(self, text="Quit", command=self.quit_game)
-        self.quit_button.pack(pady=10)
-
-    def quit_game(self):
-        self.master.quit()
+        quit_button = ctk.CTkButton(self, text="Quit", command=lambda: self.closeprogram())
+        quit_button.pack(pady=10)
+    
+    def closeprogram(self):
+        close = msg.askyesno(title="Close Now?", message="Are you sure?")
+        if close == True:
+            self.quit()
 
 
 class TeamInputFrame(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)  # Initialize the parent class
-        self.grid_columnconfigure((0, 1), weight=1)
+
+    def __init__(self, parent, controller):
+        ctk.CTkFrame.__init__(self, parent)
+        self.controller = controller
 
         # Team 1 Frame
-        self.team1_frame = ctk.CTkFrame(self)
-        self.team1_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        team1_frame = ctk.CTkFrame(self)
+        team1_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
         # Team 1 Label and Entry
-        self.team1_label = ctk.CTkLabel(self.team1_frame, text="Team 1:", )
-        self.team1_label.grid(row=0, column=0, padx=5, pady=10)
-        self.team1_label.configure(font=("Arial", 30))
-        self.team1_entry = ctk.CTkEntry(self.team1_frame, width=250, height=50)
-        self.team1_entry.grid(row=0, column=1, padx=5, pady=10)
+        team1_label = ctk.CTkLabel(team1_frame, text="Team 1:", )
+        team1_label.grid(row=0, column=0, padx=5, pady=10)   
+        team1_label.configure(font=("Arial", 30))
+        team1_entry = ctk.CTkEntry(team1_frame, width=250, height=50)
+        team1_entry.grid(row=0, column=1, padx=5, pady=10)
 
-        self.player1_label = ctk.CTkLabel(self.team1_frame, text="Player Names:")
-        self.player1_label.configure(font=("Arial", 30))
-        self.player1_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="w")
+        player1_label = ctk.CTkLabel(self.team1_frame, text="Player Names:")
+        player1_label.configure(font=("Arial", 30))
+        player1_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="w")
 
-        self.player_entries_team1 = []
+        global player_entries_team1
+        player_entries_team1 = []
         for i in range(11):
-            entry_team1 = ctk.CTkEntry(self.team1_frame, placeholder_text=f"Player {i+1}", width=250)
+            entry_team1 = ctk.CTkEntry(team1_frame, placeholder_text=f"Player {i+1}", width=250)
             entry_team1.grid(row=i+2, column=0, columnspan=2, padx=10, pady=5, sticky="we")
-            self.player_entries_team1.append(entry_team1)
+            player_entries_team1.append(entry_team1)
 
         # Team 2 Frame
-        self.team2_frame = ctk.CTkFrame(self)
-        self.team2_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+        team2_frame = ctk.CTkFrame(self)
+        team2_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
 
         # Team 2 Label and Entry
-        self.team2_label = ctk.CTkLabel(self.team2_frame, text="Team 2:")
-        self.team2_label.grid(row=0, column=0, padx=10, pady=10)
-        self.team2_label.configure(font=("Arial", 30))
-        self.team2_entry = ctk.CTkEntry(self.team2_frame, width=250, height=50)
-        self.team2_entry.grid(row=0, column=1, padx=10, pady=10)
+        team2_label = ctk.CTkLabel(self.team2_frame, text="Team 2:")
+        team2_label.grid(row=0, column=0, padx=10, pady=10)
+        team2_label.configure(font=("Arial", 30))
+        team2_entry = ctk.CTkEntry(self.team2_frame, width=250, height=50)
+        team2_entry.grid(row=0, column=1, padx=10, pady=10)
 
-        self.player2_label = ctk.CTkLabel(self.team2_frame, text="Player Names:")
-        self.player2_label.configure(font=("Arial", 30))
-        self.player2_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="w")
+        player2_label = ctk.CTkLabel(self.team2_frame, text="Player Names:")
+        player2_label.configure(font=("Arial", 30))
+        player2_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="w")
 
-        self.player_entries_team2 = []
+        global player_entries_team2
+        player_entries_team2 = []
         for i in range(11):
-            entry_team2 = ctk.CTkEntry(self.team2_frame, width=30, placeholder_text=f"Player {i+1}")
+            entry_team2 = ctk.CTkEntry(team2_frame, width=30, placeholder_text=f"Player {i+1}")
             entry_team2.grid(row=i+2, column=0, columnspan=2, padx=10, pady=5, sticky="we")
-            self.player_entries_team2.append(entry_team2)
-    
-        self.confirm_button = ctk.CTkButton(self, text="Confirm", command=self.switch_to_scoring_frame)
-        self.confirm_button.grid(row=1, column=0, columnspan=2, pady=10)        
+            player_entries_team2.append(entry_team2)
+
+
+        confirm_button = ctk.CTkButton(self, text="Confirm", command=lambda: controller.show_frame("TeamReviewFrame"))
+        confirm_button.grid(row=1, column=0, columnspan=2, pady=10)   
             
     def get_team_names(self):
         team1 = self.team1_entry.get()
@@ -132,54 +132,33 @@ class TeamInputFrame(ctk.CTkFrame):
     def get_player_names(self):
         player_names_team1 = []
         player_names_team2 = []
-        for entry_team1, entry_team2 in zip(self.player_entries_team1, self.player_entries_team2):
+        for entry_team1, entry_team2 in zip(player_entries_team1, player_entries_team2):
             name_team1 = entry_team1.get()
             name_team2 = entry_team2.get()
             player_names_team1.append(name_team1)
             player_names_team2.append(name_team2)
         return player_names_team1, player_names_team2
 
-    def switch_to_scoring_frame(self):
-        team_names = self.get_team_names()
-        player_names = self.get_player_names()
-        self.pack_forget()
-        scoring_frame = ScoringFrame(self.master, team_names, player_names)
-        scoring_frame.pack(padx=10, pady=10)
+# class TeamReviewFrame(ctk.CTkFrame):
+#     def __init__(self, parent, team_names, player_names):
+#         super().__init__(parent)
 
 
-class ScoringFrame(ctk.CTkFrame):
-    def __init__(self, master, team_names, player_names):
-        super().__init__(master)
-        self.team_names = team_names
-        self.player_names = player_names
+#         self.team_label = ctk.CTkLabel(self, text=f"Teams: {team_names[0]} vs {team_names[1]}")
+#         self.team_label.pack(pady=10)
 
-        self.team_label = ctk.CTkLabel(self, text=f"Teams: {team_names[0]} vs {team_names[1]}")
-        self.team_label.pack(pady=10)
+#         self.player_label = ctk.CTkLabel(self, text=f"Players: {player_names[0]} vs {player_names[1]}")
+#         self.player_label.pack(pady=10)
 
-        self.player_label = ctk.CTkLabel(self, text=f"Players: {player_names[0]} vs {player_names[1]}")
-        self.player_label.pack(pady=10)
+#         self.back_button = ctk.CTkButton(self, text="Back", command=)
+#         self.back_button.pack(pady=10)
 
-        self.back_button = ctk.CTkButton(self, text="Back", command=self.switch_to_team_input_frame)
-        self.back_button.pack(pady=10)
+#         self.next_button = ctk.CTkButton(self, text="Next", command=)
+#         self.next_button.pack(pady=10)
 
-        self.next_button = ctk.CTkButton(self, text="Next", command=self.switch_to_toss_frame)
-        self.next_button.pack(pady=10)
-
-        self.back_to_toss_callback = None  # Initialize the callback function variable
-        
-    def switch_to_team_input_frame(self):
-        self.pack_forget()
-        team_input_frame = TeamInputFrame(self.master)
-        team_input_frame.pack(padx=10, pady=10)
-   
-    def switch_to_toss_frame(self):
-        if self.back_to_toss_callback:
-            self.back_to_toss_callback(self.team_names)  # Pass team_names back to the toss frame
-            
-class TossFrame(ctk.CTkFrame):
-    def __init__(self, master, team_names):
-        super().__init__(master)
-        self.team_names = team_names
+# class TossFrame(ctk.CTkFrame):
+    def __init__(self, parent, team_names):
+        super().__init__(parent)
 
         self.toss_label = ctk.CTkLabel(self, text="Toss Time! Click the button to determine batting and bowling teams.")
         self.toss_label.pack(pady=10)
@@ -198,4 +177,4 @@ class TossFrame(ctk.CTkFrame):
 
 # Create and run the application
 app = App()
-app.run()
+app.mainloop()
